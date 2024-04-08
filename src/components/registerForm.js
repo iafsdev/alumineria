618aea8f4e37c4ceb5c2bb4742470ac2a9bdb10b
iafs32 <iafs.0232@gmail.com>
2024-04-08 06:17:31 -0600

    Añade autenticación de firebase al registro

--- src/components/registerForm.js
+++ src/components/registerForm.js
@@ -1,16 +1,38 @@
+import { auth } from "@/auth/firebase"
+import { createUserWithEmailAndPassword } from 'firebase/auth'
+
 export default function RegisterForm() {
+
+  const navigate = useNavigate();
+
+  const [email, setEmail] = useState('')
+  const [password, setPassword] = useState('');
   
+  const onSubmit = async (e) => {
+    e.preventDefault()
+    await createUserWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          const user = userCredential.user;
+          console.log(user);
+          navigate("/login")
+      })
+      .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage);
+      });
+  }
 
   return (
     <div className="div-registerForm">
       <h2 className="login-title">Registro</h2>
       <form className="register-form">
         <input className="login-input" name="name" type="text" placeholder="Nombre completo"/>
-        <input className="login-input" name='email' type="email" placeholder="Correo"/>
-        <input className="login-input" name="password" type="password" placeholder="Contraseña"/>
+        <input className="login-input" name='email' type="email" placeholder="Correo" onChange={(e) => setEmail(e.target.value)}/>
+        <input className="login-input" name="password" type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)}/>
         <input className="login-input" name='phone' type='tel' placeholder="Teléfono"/>
         <input className="login-input" name="address" type='text' placeholder="Dirección"/>
-        <button className="login-button">Registrarse</button>
+        <button className="login-button" onClick={onSubmit}>Registrarse</button>
       </form>
     </div>
   )
