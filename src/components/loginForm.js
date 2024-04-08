2383ebc948b2f58a574b871dc5970dd964fa2ab5
iafs32 <iafs.0232@gmail.com>
2024-04-08 06:20:24 -0600

    Añade la autenticación de firebase al login

--- src/components/loginForm.js
+++ src/components/loginForm.js
@@ -1,11 +1,33 @@
+import { auth } from "@/auth/firebase"
+import { signInWithEmailAndPassword } from 'firebase/auth'
+
 export default function LoginForm() {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const onLogin = (e) => {
+      e.preventDefault();
+      signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+          const user = userCredential.user;
+          navigate("/register")
+          console.log(user);
+      })
+      .catch((error) => {
+          const errorCode = error.code;
+          const errorMessage = error.message;
+          console.log(errorCode, errorMessage)
+      });
+  }
+
   return (
     <div className="div-loginForm">
       <h2 className="login-title">Iniciar sesión</h2>
       <form className="login-form">
-        <input className="login-input" name='email' type="email" placeholder="Correo"/>
-        <input className="login-input" name="password" type="password" placeholder="Contraseña"/>
-        <button className="login-button">Ingresar</button>
+        <input className="login-input" name='email' type="email" placeholder="Correo" onChange={(e) => setEmail(e.target.value)}/>
+        <input className="login-input" name="password" type="password" placeholder="Contraseña" onChange={(e) => setPassword(e.target.value)}/>
+        <button className="login-button" onClick={onLogin}>Ingresar</button>
       </form>
     </div>
   )
