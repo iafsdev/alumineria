0b9061aee3cd815d334703053a438fbb6ca3d6a5
iafs32 <iafs.0232@gmail.com>
2024-04-08 06:10:09 -0600

    Añade el archivo de configuración de firebase

---
+++ src/auth/firebase.js
@@ -0,0 +1,22 @@
+// firebase-auth.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-app.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-analytics.js";
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.8.1/firebase-auth.js";
+
+
+// Configuración de Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyBSKb7zmWCn4GGdwmmPJM1xZif_pk8-j5M",
+    authDomain: "alusmart-e65a1.firebaseapp.com",
+    projectId: "alusmart-e65a1",
+    storageBucket: "alusmart-e65a1.appspot.com",
+    messagingSenderId: "624493076221",
+    appId: "1:624493076221:web:827bc4c640d646b7d9a894",
+    measurementId: "G-38G7Y0E0RW"
+  };
+
+// Inicializar Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+export const auth = getAuth(app);
+export default app;
